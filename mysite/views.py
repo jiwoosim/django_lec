01c57fa40a71abc013ec7558830cad5a490de0fc
jiwoosim <jiwoosim@naver.com>
2022-05-21 23:37:24 +0900

    2차 django 개발 시작

---
+++ mysite/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import get_object_or_404, render
+from .models import MainContent
+def detail(request, content_id):
+ content_list = get_object_or_404(MainContent, pk=content_id)
+ context = { 'content_list': content_list}
+ return render(request, 'mysite/content_detail.html', context)
